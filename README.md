ce033fea3e0d8993be7d8258ada705c4f67018bc
Conrad Halling <conrad.halling@icloud.com>
2023-03-02 11:58:55 -0500

    Create the application factory
    Complete the Application Setup instructions at
    https://flask.palletsprojects.com/en/2.2.x/tutorial/factory/.
    
    Create the application factory and display "Hello, World!"
    at http://127.0.0.1:5000/hello.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # flask_tutorial
- Flask 2.2 tutorial
+
+## Description
+
+This code implements the application created by the Flask 2.2 tutorial at
+https://flask.palletsprojects.com/en/2.2.x/tutorial/.
+
+## Starting the Application
+
+Run the application with the following command:
+
+```shell
+flask --app flaskr run --debug
+```
+
+## Version 0.1
+
+This version completes the Application Setup instructions at
+https://flask.palletsprojects.com/en/2.2.x/tutorial/factory/.
+
+The application displays "Hello, World!" at the URL
+http://127.0.0.1:5000/hello.
---
+++ flaskr/__init__.py
@@ -0,0 +1,37 @@
+"""
+
+"""
+
+import os
+
+import flask
+
+
+def create_app(test_config=None):
+    # Create and configure the application.
+    app = flask.Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+    )
+
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing.
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # Load the test config if passed in.
+        app.config.from_mapping(test_config)
+
+    # Ensure the instance folder exists. This should be outside the flaskr
+    # package.
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Add a simple page that says "hello".
+    @app.route("/hello")
+    def hello():
+        return "Hello, World!"
+
+    return app
